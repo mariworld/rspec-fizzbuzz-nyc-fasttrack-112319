539f1241a11ed61cdf9e8f822fd37d90198d2482
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-23 19:08:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+require_relative '../fizzbuzz.rb'
+def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+        puts "FizzBuzz"
+    elsif num % 3 == 0
+        puts "Fizz"
+    elsif num % 5 == 0
+        puts "Buzz"
+    end
+end
